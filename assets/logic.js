942d9b9992a42c4cb8f9f7dcd591a5dfad93fb56
Lena Pan <lena@Lenas-MacBook-Air.local>
2021-08-10 15:01:30 -0700

    Reduced 63 lines of codeby learning/ adding a forEach loop. Included score at the end

--- assets/logic.js
+++ assets/logic.js
@@ -14,11 +14,11 @@ var start = 0;
 
 function setTime() {
   var timerInterval = setInterval(function() {
-    start++;
+    start--;
     countDown.style.color="red";
     countDown.textContent = "Time: " + start;
 
-  }, 1000);
+  }, 555);
 }
 
 /************************************************** */
@@ -28,10 +28,11 @@ var b = document.getElementById("b");
 var c = document.getElementById("c");
 var d = document.getElementById("d");
 // index for looping through the quiz array of question objects
-var i = 1; //We're starting at 1 because line 47-51 already starts at index 0. Once user selects a choice, the next question to appear needs to be in index 1, not 0.          
-var answer = 0; // see line 120 for the purpose of this variable
+var i = 1; //We're declaring this at 1 for the "next click" or  next question because when user hits "Start Quiz," it already starts at index 0. Once user selects a choice, the next question to appear needs to be in index 1, not 0.          
+var answer = 0; // to compare what user selects VS the correct answer in each question object in the quiz array
 var correct = 0;
 var end = document.getElementById("end-screen");
+var score = document.getElementById("final-score");
 
 function displayQuestions(){
   var q = document.getElementById("questions");
@@ -49,10 +50,12 @@ function displayQuestions(){
     b.textContent = quiz[0].b;
     c.textContent = quiz[0].c;
     d.textContent = quiz[0].d;
-  
-  
-  a.addEventListener("click", function(){
-  if (i < quiz.length) {
+
+
+
+[a,b,c,d].forEach((button)=>{
+  button.addEventListener('click',function(){
+    if (i < quiz.length) {
     title.textContent = quiz[i].quest;
     a.textContent = quiz[i].a;
     b.textContent = quiz[i].b;
@@ -63,62 +66,17 @@ function displayQuestions(){
     }else{
       q.style.display = "none";
       end.style.display = "block";
-    }
+      score.innerHTML = correct + " out of " + quiz.length;
+      }  
+    });
   });
-
-  b.addEventListener("click", function(){
-  if (i < quiz.length) {
-    title.textContent = quiz[i].quest;
-    a.textContent = quiz[i].a;
-    b.textContent = quiz[i].b;
-    c.textContent = quiz[i].c;
-    d.textContent = quiz[i].d;
-    i++;
-    answer++;
-    } 
-    else{
-    q.style.display = "none";
-    end.style.display = "block";
-    }
-  });
-
-  c.addEventListener("click", function(){
-  if (i < quiz.length) {
-    title.textContent = quiz[i].quest;
-    a.textContent = quiz[i].a;
-    b.textContent = quiz[i].b;
-    c.textContent = quiz[i].c;
-    d.textContent = quiz[i].d;
-    i++;
-    answer++; 
-    } else{
-    q.style.display = "none";
-    end.style.display = "block";
-    }
-  });
-  d.addEventListener("click", function(){
-  if (i < quiz.length) {
-    title.textContent = quiz[i].quest;
-    a.textContent = quiz[i].a;
-    b.textContent = quiz[i].b;
-    c.textContent = quiz[i].c;
-    d.textContent = quiz[i].d;
-    i++;
-    answer++;
-    } else{
-    q.style.display = "none";
-    end.style.display = "block";
-    }
-  }); 
-
-  } 
-var score = document.getElementById("final-score");
+ 
+} 
 
 function checkABCD(userSelect){
   if(userSelect == quiz[answer].answer){   // see questions.js file to check out the quiz objects and the answers to the multiple choice questions
     correct++;
-    console.log(correct +" Correct. " + "Answer is " + quiz[answer].answer);
-   // score.push(valueOf(correct));
+    console.log(correct +" Correct." + " Answer is " + quiz[answer].answer);
     console.log("You selected " + userSelect);
   }else { 
     console.log("Wrong. " + "Answer is " + quiz[answer].answer);
