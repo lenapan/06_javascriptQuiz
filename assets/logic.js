d8eee7015914f87741e522bce270f5315eb9a49f
Lena Pan <lena@Lenas-MacBook-Air.local>
2021-08-10 15:31:15 -0700

    Fixed timer. Condensed code. Reduced 63 lines of code using forEach alone.

--- assets/logic.js
+++ assets/logic.js
@@ -1,16 +1,5 @@
-/*
-$(document).ready(function(){
-
-	$("#start").on("click", function(){
-    $(".timer").empty();
-  });
-
-});
-*/
-
-/************************************************** */
 var countDown = document.querySelector(".timer");
-var start = 0;
+var start = 555;
 
 function setTime() {
   var timerInterval = setInterval(function() {
@@ -18,15 +7,15 @@ function setTime() {
     countDown.style.color="red";
     countDown.textContent = "Time: " + start;
 
-  }, 555);
+  }, 1000);
 }
-
 /************************************************** */
 //assigning multiple choice buttons to a variable
 var a = document.getElementById("a");
 var b = document.getElementById("b");
 var c = document.getElementById("c");
 var d = document.getElementById("d");
+
 // index for looping through the quiz array of question objects
 var i = 1; //We're declaring this at 1 for the "next click" or  next question because when user hits "Start Quiz," it already starts at index 0. Once user selects a choice, the next question to appear needs to be in index 1, not 0.          
 var answer = 0; // to compare what user selects VS the correct answer in each question object in the quiz array
@@ -51,28 +40,24 @@ function displayQuestions(){
     c.textContent = quiz[0].c;
     d.textContent = quiz[0].d;
 
-
-
-[a,b,c,d].forEach((button)=>{
-  button.addEventListener('click',function(){
-    if (i < quiz.length) {
-    title.textContent = quiz[i].quest;
-    a.textContent = quiz[i].a;
-    b.textContent = quiz[i].b;
-    c.textContent = quiz[i].c;
-    d.textContent = quiz[i].d;
-    i++;
-    answer++;   
-    }else{
-      q.style.display = "none";
-      end.style.display = "block";
-      score.innerHTML = correct + " out of " + quiz.length;
-      }  
+  [a,b,c,d].forEach((button)=>{
+    button.addEventListener('click',function(){
+      if (i < quiz.length) {
+      title.textContent = quiz[i].quest;
+      a.textContent = quiz[i].a;
+      b.textContent = quiz[i].b;
+      c.textContent = quiz[i].c;
+      d.textContent = quiz[i].d;
+      i++;
+      answer++;   
+      }else{
+        q.style.display = "none";
+        end.style.display = "block";
+        score.innerHTML = correct + " out of " + quiz.length;
+        }  
     });
   });
- 
 } 
-
 function checkABCD(userSelect){
   if(userSelect == quiz[answer].answer){   // see questions.js file to check out the quiz objects and the answers to the multiple choice questions
     correct++;
